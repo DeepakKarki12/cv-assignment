4bb59e72b5208b23932a65c4963face03dd25a33
Deepak Karki <88586113+DeepakKarki12@users.noreply.github.com>
2022-04-28 08:33:54 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import cv2
 import matplotlib.pyplot as plt
 
 # read the image file in a grayscale mode
-img = cv2.imread('r11.png',0)
+img = cv2.imread('r9f.png',0)
 
 # variable declaration
 sum = 0
@@ -15,10 +15,15 @@ for i in img:
         count+=1
 avg = int(sum/count)
 
-# converting the grayscale image into binary
-ret, bw_img = cv2.threshold(img, avg, 1, cv2.THRESH_BINARY_INV)
-plt.imshow(bw_img)
+# converting the grayscale image into binary_inv so that we can the foreground as 1 and background as 0
+ret, bw_img = cv2.threshold(img, avg,255, cv2.THRESH_BINARY_INV)
+
+#now converting the gray image to bgr so that we can get the black and white image
+black_and_white = cv2.cvtColor(bw_img,cv2.COLOR_GRAY2BGR)
+
+plt.imshow(black_and_white)
 plt.show()
 print(bw_img)
+
 cv2.waitKey(0)
 cv2.destroyAllWindows()
